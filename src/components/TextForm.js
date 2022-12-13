297096b5555abab81a1fed5576109d7c717c80d4
Aryan Gupta <guptaaryan2401@gmail.com>
2022-12-13 15:40:54 +0530

    new changes , fixed some bugs

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -26,6 +26,7 @@ export default function TextForm(props) {
     var text = document.getElementById("myBox");
     text.select();
     navigator.clipboard.writeText(text.value);
+    document.getSelection().removeAllRanges();
     props.showAlert("Text is copied to clipboard!!", "success");
   };
 
@@ -69,28 +70,43 @@ export default function TextForm(props) {
             rows="8"
           ></textarea>
         </div>
-        <button className="btn btn-primary mx-2 my-1" onClick={handleUpClick}>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-primary mx-2 my-1"
+          onClick={handleUpClick}
+        >
           Covert to Uppercase
         </button>
-        <button className="btn btn-primary mx-2 my-1" onClick={handleLoClick}>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-primary mx-2 my-1"
+          onClick={handleLoClick}
+        >
           Covert to Lowercase
         </button>
         <button
+          disabled={text.length === 0}
           className="btn btn-primary mx-2 my-1"
           onClick={handleInvertClick}
         >
           Invertcases
         </button>
-        <button className="btn btn-primary mx-2 my-1" onClick={handleCopy}>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-primary mx-2 my-1"
+          onClick={handleCopy}
+        >
           Copy Text
         </button>
         <button
+          disabled={text.length === 0}
           className="btn btn-primary mx-2 my-1"
           onClick={handleExtraSpace}
         >
           Remove Extra Space
         </button>
         <button
+          disabled={text.length === 0}
           className="btn btn-primary mx-2 my-1"
           onClick={handleClearClick}
         >
@@ -120,11 +136,7 @@ export default function TextForm(props) {
           Minutes read
         </p>
         <h3>Preview</h3>
-        <p>
-          {text.length > 0
-            ? text
-            : "Enter something in text box above to preview it here"}
-        </p>
+        <p>{text.length > 0 ? text : "Nothing to preview."}</p>
       </div>
     </>
   );
